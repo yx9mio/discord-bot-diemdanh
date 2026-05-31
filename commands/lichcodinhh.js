0b8aeae8d0e5cce7d2d247a12d5685017d9fa30e
Mio <87184289+yx9mio@users.noreply.github.com>
2026-05-31 11:25:17 +0700

    chore: remove lichcodinhh.js (merged into lichcodinh.js)

--- commands/lichcodinhh.js
+++
@@ -1,4 +0,0 @@
-// FILE NÀY ĐÃ ĐƯỢC MERGE VÀO commands/lichcodinh.js
-// Xóa file này khỏi commandHandler bằng cách để nó không export data/execute hợp lệ
-// (file giữ lại để git history, không load vào bot)
-module.exports = {};
